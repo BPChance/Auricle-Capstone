51daeb8d2791c7992e612b3c44f0d2160fe63076
BPChance <bradenpchance6@gmail.com>
2025-07-30 18:56:37 -0500

    fixed type error

--- my-app/src/app/exercise/[id]/page.tsx
+++ my-app/src/app/exercise/[id]/page.tsx
@@ -2,11 +2,11 @@ import { allExercises } from "@/data/allExercises";
 import { notFound } from "next/navigation";
 import ExercisePlayer from "@/components/ExercisePlayer";
 
-type Props = {
+export default async function ExercisePage({
+  params,
+}: {
   params: { id: string };
-};
-
-export default async function ExercisePage({ params }: Props) {
+}) {
   const exercise = allExercises[params.id as keyof typeof allExercises];
 
   if (!exercise) return notFound();
