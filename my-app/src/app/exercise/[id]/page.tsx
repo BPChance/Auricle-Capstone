f81ee65e9b4af09097aa97e446501c2250930302
BPChance <bradenpchance6@gmail.com>
2025-07-30 19:24:06 -0500

    fixing

--- my-app/src/app/exercise/[id]/page.tsx
+++ my-app/src/app/exercise/[id]/page.tsx
@@ -2,11 +2,7 @@ import { allExercises } from "@/data/allExercises";
 import { notFound } from "next/navigation";
 import ExercisePlayer from "@/components/ExercisePlayer";
 
-type Props = {
-  params: { id: string };
-};
-
-export default async function ExercisePage({ params }: Props) {
+export default function ExercisePage({ params }: { params: { id: string } }) {
   const exercise = allExercises[params.id as keyof typeof allExercises];
 
   if (!exercise) return notFound();
