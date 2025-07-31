0e78f87b24201af03bedddec4cd23c32d6e89d45
BPChance <bradenpchance6@gmail.com>
2025-07-31 13:15:43 -0500

    changing prop

--- my-app/src/app/exercise/[id]/page.tsx
+++ my-app/src/app/exercise/[id]/page.tsx
@@ -2,11 +2,11 @@ import { allExercises } from "@/data/allExercises";
 import { notFound } from "next/navigation";
 import ExercisePlayer from "@/components/ExercisePlayer";
 
-type PageProps = {
+type Props = {
   params: { id: string };
 };
 
-export default function ExercisePage({ params }: PageProps) {
+export default function ExercisePage({ params }: Props) {
   const exercise = allExercises[params.id as keyof typeof allExercises];
 
   if (!exercise) return notFound();
